a131ff8b5c96242171fdaa95b2ef3bb8081196a0
harrison-burchfield <harry.burchfield@gmail.com>
2025-10-28 16:17:08 -0400

    Comment out smoothie response display and add dataframe
    Comment out the line displaying the smoothie response and add a dataframe to show the response data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col,when_matched
 import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -45,4 +45,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
